7c97f7f059603ec3122a2ff19d939de5c55ce411
Tagbo Chinedu <johntagbo2@gmail.com>
2022-09-03 13:06:16 +0100

    filter logic

--- src/Pages/Products/Men/MaleProductItems.js
+++ src/Pages/Products/Men/MaleProductItems.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ImageGrid } from "../../../Resources/Image";
 
 const MaleProductItems = () => {
   const params = useParams();
-  return <p>{params.id}</p>;
+  const [menProductItem, setMenProductItem] = useState([]);
+  useEffect(() => {
+    setMenProductItem(
+      ImageGrid.filter((item) => {
+        return params.id === item.id;
+      })
+    );
+  }, [params.id]);
+
+  return (
+    <div className="px-6 py-4">
+      {menProductItem.map((menItem) => {
+        return (
+          <div key={menItem.id}>
+            <h1 className="text-lg font-semibold underline">
+              Men{">"}
+              {menItem.name}
+            </h1>
+            <div className="flex justify-evenly">
+              <div className="w-5/12">
+                <img
+                  clasName="w-full h-auto"
+                  src={menItem.url}
+                  alt={menItem.name}
+                />
+              </div>{" "}
+              <div className='w-5/12'><h1 className='text-xl '>{menItem.name}</h1></div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default MaleProductItems;
